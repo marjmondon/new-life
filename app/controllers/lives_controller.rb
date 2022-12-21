6e3581a8e6e6a2220e884ec4a4077029384f90b1
Marjorie MONDON <marjmondon@hotmail.com>
2022-12-21 13:27:30 -0500

    pundit set up ok

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -1,10 +1,37 @@
 class LivesController < ApplicationController
+  def index
+    @lives = policy_scope(Life)
+  end
+
   def create
     @life = Life.new(life_params)
     # The user of the life to create is the current user
     @life.user = current_user
-    # [...]
+    authorize @life
   end
+
+  def new
+    @life = Life.new
+    authorize @life
+  end
+
+  def show
+    authorize @life
+  end
+
+  def edit
+    authorize @life
+  end
+
+  def update
+    authorize @life
+  end
+
+  def destroy
+    authorize @life
+  end
+
+  private
 
   def life_params
     params.require(:life).permit(:title, :description, :price )
--- app/policies/life_policy.rb
+++ app/policies/life_policy.rb
@@ -1,8 +1,27 @@
 class LifePolicy < ApplicationPolicy
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  # only the owner of a life can update and destroy
+  def update?
+    record.user == user
+    # record: the life passed to the `authorize` method in controller
+    # user: the `current_user` signed in with Devise
+  end
+
+  def destroy?
+    record.user == user
+  end
+
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
   end
 end
